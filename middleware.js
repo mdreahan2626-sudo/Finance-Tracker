62aceac3fc4c4e1443440a6b26c28297129760b8
rehan24bce11230-hash <rehan.24bce11230@vitbhopal.ac.in>
2026-07-05 10:40:18 +0530

    chore: remove arcjet from middleware to reduce edge function size below Vercel limit

--- middleware.js
+++ middleware.js
@@ -1,4 +1,3 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -8,20 +7,7 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-const isInngestRoute = createRouteMatcher(["/api/inngest(.*)", "/api/test-send(.*)"]);
-
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
-    }),
-  ],
-});
-
-const clerk = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
@@ -29,13 +15,6 @@ const clerk = clerkMiddleware(async (auth, req) => {
   }
   return NextResponse.next();
 });
-
-export default async function middleware(req) {
-  if (isInngestRoute(req)) {
-    return NextResponse.next();
-  }
-  return createMiddleware(aj, clerk)(req);
-}
 
 export const config = {
   matcher: [
