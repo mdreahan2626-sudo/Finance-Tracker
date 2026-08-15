0ed8ff370c849574a9e4d19c5c43eba5b5a06f8c
rehan24bce11230-hash <rehan.24bce11230@vitbhopal.ac.in>
2026-08-15 17:52:45 +0530

    Fix middleware size limit by replacing it with Server Component auth

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const MainLayout = ({ children }) => {
+const MainLayout = async ({ children }) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   return <div className="container mx-auto my-32">{children}</div>;
 };
 
